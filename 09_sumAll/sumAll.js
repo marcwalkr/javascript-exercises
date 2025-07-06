92d02c7634c20974f7ec5a4ead9c604d3c1a6b4a
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-07-06 14:24:11 -0500

    Implement sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = start; i >= end; i--) {
+      sum += i;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
