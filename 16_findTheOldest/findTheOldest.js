cf894a0d699cfbc909879186685d4680ada86d05
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-07-16 15:27:07 -0500

    Implement findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  const currentYear = new Date().getFullYear();
+  return (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    const currentAge = getAge(current);
+    const oldestAge = getAge(oldest);
+    return currentAge > oldestAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
