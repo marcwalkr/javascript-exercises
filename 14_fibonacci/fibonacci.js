6e2344500a132c3024c985ab76069638670bca3d
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-07-16 13:18:42 -0500

    Implement fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (isNaN(Number(n)) || Number(n) < 0) return "OOPS";
+  if (Number(n) === 0) return 0;
+  if (Number(n) < 3) return 1;
 
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
